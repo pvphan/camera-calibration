3907d16b5c9dc6c330a1ebb8bb94a612283d89b2
Vinh Phan <pvphan90@gmail.com>
2022-01-09 13:13:52 -0800

    storing functions in class after init (slow)

--- src/jacobian.py
+++ src/jacobian.py
@@ -13,38 +13,77 @@ class ProjectionJacobian:
             self._uvExpr = createExpressionIntrinsicProjectionRadTan()
             self._intrinsicSymbols = getIntrinsicRadTanSymbols()
             self._intrinsicJacobianBlockExpr = self._createJacobianBlockExpression(self._intrinsicSymbols)
+            self._intrinsicJacobianBlockFunctions, self._intrinsicJacobianBlockInputSymbols = \
+                    self._createJacobianBlockFunctions(self._intrinsicJacobianBlockExpr)
+
             self._extrinsicSymbols = getExtrinsicSymbols()
             self._extrinsicJacobianBlockExpr = self._createJacobianBlockExpression(self._extrinsicSymbols)
+            self._extrinsicJacobianBlockFunctions, self._extrinsicJacobianBlockInputSymbols = \
+                    self._createJacobianBlockFunctions(self._extrinsicJacobianBlockExpr)
         else:
             raise NotImplementedError("Only radial-tangential distortion supported currently")
 
+
+    def _createJacobianBlockFunctions(self, jacobianBlockExpr):
+        inputSymbols = []
+        jacobianBlockFunctions = []
+        for i in range(jacobianBlockExpr.shape[0]):
+            rowInputSymbols = []
+            rowJacobianBlockFunctions = []
+            for j in range(jacobianBlockExpr.shape[1]):
+                expression = jacobianBlockExpr[i, j]
+                lambdaFunction, orderedInputSymbosl = createLambdaFunction(expression)
+                rowJacobianBlockFunctions.append(lambdaFunction)
+                rowInputSymbols.append(orderedInputSymbosl)
+
+            jacobianBlockFunctions.append(rowJacobianBlockFunctions)
+            inputSymbols.append(rowInputSymbols)
+        return np.array(jacobianBlockFunctions), np.array(inputSymbols)
+
     def _createIntrinsicsJacobianBlock(self, intrinsicValues, extrinsicValues, modelPoint):
-        intrinsicBlock = np.zeros(shape=self._intrinsicJacobianBlockExpr.shape)
         valuesDict = self._createValuesDict(intrinsicValues, extrinsicValues, modelPoint)
-        for i, symbol in enumerate(self._intrinsicSymbols):
-            du = self._intrinsicJacobianBlockExpr[0,i].evalf(subs=valuesDict)
-            dv = self._intrinsicJacobianBlockExpr[1,i].evalf(subs=valuesDict)
-            intrinsicBlock[0,i] = du
-            intrinsicBlock[1,i] = dv
+        intrinsicBlock = np.zeros(shape=self._intrinsicJacobianBlockFunctions.shape)
+        for i in range(self._intrinsicJacobianBlockFunctions.shape[0]):
+            for j in range(self._intrinsicJacobianBlockFunctions.shape[1]):
+                intrinsicBlock[i,j] = evaluate(self._intrinsicJacobianBlockFunctions[i,j],
+                        self._intrinsicJacobianBlockInputSymbols[i,j], valuesDict)
         return intrinsicBlock
 
     def _createExtrinsicsJacobianBlock(self, intrinsicValues, extrinsicValues, modelPoint):
-        extrinsicBlock = np.zeros(shape=self._extrinsicJacobianBlockExpr.shape)
-        valuesDict = self._createValuesDict(intrinsicValues, extrinsicValues, modelPoint)
-        for i, symbol in enumerate(self._extrinsicSymbols):
-            du = self._extrinsicJacobianBlockExpr[0,i].evalf(subs=valuesDict)
-            dv = self._extrinsicJacobianBlockExpr[1,i].evalf(subs=valuesDict)
-            extrinsicBlock[0,i] = du
-            extrinsicBlock[1,i] = dv
+        valuesDict = self._createValuesDict(extrinsicValues, extrinsicValues, modelPoint)
+        extrinsicBlock = np.zeros(shape=self._extrinsicJacobianBlockFunctions.shape)
+        for i in range(self._extrinsicJacobianBlockFunctions.shape[0]):
+            for j in range(self._extrinsicJacobianBlockFunctions.shape[1]):
+                extrinsicBlock[i,j] = evaluate(self._extrinsicJacobianBlockFunctions[i,j],
+                        self._extrinsicJacobianBlockInputSymbols[i,j], valuesDict)
         return extrinsicBlock
 
     def _createValuesDict(self, intrinsicValues, extrinsicValues, modelPoint):
+        """
+        Input:
+            intrinsicValues -- α, β, γ, uc, uv, k1, k2, p1, p2, k3
+            extrinsicValues -- ρx, ρy, ρz, tx, ty, tz
+            modelPoint -- X0, Y0, Z0
+
+        Output:
+            valuesDict -- dictionary with items (key=symbol, value=value)
+        """
         valuesDict = dict(zip(self._intrinsicSymbols, intrinsicValues))
         valuesDict.update(dict(zip(self._extrinsicSymbols, extrinsicValues)))
         insertModelPoints(valuesDict, modelPoint)
         return valuesDict
 
     def _createJacobianBlockExpression(self, derivativeSymbols):
+        """
+        Input:
+            derivativeSymbols -- the symbols with which to take the partial derivative
+                    of the projection expression (left-to-right along column dimension)
+
+        Output:
+            jacobianBlockExpr -- matrix containing the expressions for the partial
+                    derivative of the point projection expression wrt the corresponding
+                    derivative symbol
+        """
         uExpr, vExpr = self._uvExpr.ravel()
         uExprs = []
         vExprs = []
@@ -76,13 +115,19 @@ class ProjectionJacobian:
 
 
 def insertModelPoints(valuesDict, modelPoint):
-    X0, Y0, Z0 = sympy.symbols("X0 Y0 Z0")
+    X0, Y0, Z0 = getModelPointSymbols()
     valuesDict[X0] = modelPoint[0]
     valuesDict[Y0] = modelPoint[1]
     valuesDict[Z0] = modelPoint[2]
 
 
 def createExpressionIntrinsicProjectionRadTan():
+    """
+    Creates the base expression for point projection (u, v) from P vector symbols
+    and a single world point wP = (X0, Y0, Z0), with
+            P = (α, β, γ, uc, uv, k1, k2, p1, p2, k3, ρx, ρy, ρz, tx, ty, tz)
+    """
+    isSymbolic = True
     α, β, γ, uc, vc, k1, k2, p1, p2, k3 = getIntrinsicRadTanSymbols()
     A = np.array([
         [α, γ, uc],
@@ -90,19 +135,19 @@ def createExpressionIntrinsicProjectionRadTan():
         [0, 0,  1],
     ])
     ρx, ρy, ρz, tx, ty, tz = getExtrinsicSymbols()
-    R = mu.eulerToRotationMatrix((ρx, ρy, ρz), isSymbolic=True)
+    R = mu.eulerToRotationMatrix((ρx, ρy, ρz), isSymbolic=isSymbolic)
     cMw = np.array([
         [R[0,0], R[0,1], R[0,2], tx],
         [R[1,0], R[1,1], R[1,2], ty],
         [R[2,0], R[2,1], R[2,2], tz],
         [     0,      0,      0,  1],
     ])
-    X0, Y0, Z0 = sympy.symbols("X0 Y0 Z0")
-    wP = mu.col((X0, Y0, Z0, 1))
-    cPHom = (cMw @ wP).T
-    X0 = mu.unhom(cPHom)
+    X0, Y0, Z0 = getModelPointSymbols()
+    wPHom = mu.col((X0, Y0, Z0, 1))
+    cPHom = (cMw @ wPHom).T
+    cP = mu.unhom(cPHom)
     k = (k1, k2, p1, p2, k3)
-    uvExpr = distortion.projectWithDistortion(A, X0, k, isSymbolic=True)
+    uvExpr = distortion.projectWithDistortion(A, cP, k, isSymbolic=isSymbolic)
     return uvExpr
 
 
@@ -112,3 +157,28 @@ def getIntrinsicRadTanSymbols():
 
 def getExtrinsicSymbols():
     return sympy.symbols("ρx ρy ρz tx ty tz")
+
+
+def getModelPointSymbols():
+    return sympy.symbols("X0 Y0 Z0")
+
+
+def getAllSymbolsOrdered():
+    intrinsicSymbols = getIntrinsicRadTanSymbols()
+    extrinsicSymbols = getExtrinsicSymbols()
+    modelPointSymbols = getModelPointSymbols()
+    return intrinsicSymbols + extrinsicSymbols + modelPointSymbols
+
+
+def createLambdaFunction(expression):
+    orderedSymbols = tuple(expression.atoms(sympy.Symbol))
+    f = sympy.lambdify(orderedSymbols, expression, "numpy")
+    return f, orderedSymbols
+
+
+def evaluate(f, orderedSymbols, valuesDict):
+    eps = 1e-100
+    orderedInputs = [valuesDict[symbol] if np.abs(valuesDict[symbol]) > eps else eps
+            for symbol in orderedSymbols]
+    output = f(*orderedInputs)
+    return output
--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -1,42 +1,48 @@
 import unittest
 
 import numpy as np
+import sympy
 
 from __context__ import src
 from src import distortion
 from src import jacobian
+from src import mathutils as mu
 
 
-class TestJacobian(unittest.TestCase):
+# all tests take 11 sec with evalf(subs=...)
+# takes ~7 sec with lambdify
+class TestProjectionJacobian(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         distortionModel = distortion.DistortionModel.RadialTangential
         cls.jac = jacobian.ProjectionJacobian(distortionModel)
+        cls.intrinsicValues = [400, 400, 0, 320, 240, -0.5, 0.2, 0, 0, 0]
+        cls.extrinsicValues = [180, 0, 0, 0.1, 0.2, 1.0]
+        cls.modelPoint = [0.1, 0.1, 0]
 
     def test_init(self):
         self.assertEqual(self.jac._intrinsicJacobianBlockExpr.shape, (2, 10))
+        self.assertEqual(self.jac._intrinsicJacobianBlockFunctions.shape, (2, 10))
+        self.assertEqual(self.jac._intrinsicJacobianBlockInputSymbols.shape, (2, 10))
+
         self.assertEqual(self.jac._extrinsicJacobianBlockExpr.shape, (2, 6))
+        self.assertEqual(self.jac._extrinsicJacobianBlockFunctions.shape, (2, 6))
+        self.assertEqual(self.jac._extrinsicJacobianBlockInputSymbols.shape, (2, 6))
 
     def test_createExpressionIntrinsicProjection(self):
         expr = jacobian.createExpressionIntrinsicProjectionRadTan()
         self.assertNotEqual(str(expr), "None")
 
     def test__createIntrinsicsJacobianBlock(self):
-        intrinsicValues = [400, 400, 0, 320, 240, -0.5, 0.2, 0, 0, 0]
-        extrinsicValues = [180, 0, 0, -0.5, 0.2, 1.0]
-        modelPoint = (0.1, 0.3, 0)
-        intrinsicBlock = self.jac._createIntrinsicsJacobianBlock(intrinsicValues,
-                extrinsicValues, modelPoint)
+        intrinsicBlock = self.jac._createIntrinsicsJacobianBlock(self.intrinsicValues,
+                self.extrinsicValues, self.modelPoint)
         self.assertEqual(intrinsicBlock.shape, (2, 10))
         self.assertNoNans(intrinsicBlock)
         self.assertNonZero(intrinsicBlock)
 
     def test__createExtrinsicsJacobianBlock(self):
-        intrinsicValues = [400, 400, 0, 320, 240, -0.5, 0.2, 0, 0, 0]
-        extrinsicValues = [180, 0, 0, 0.1, 0.2, 1.0]
-        modelPoint = (0.1, 0.1, 0)
-        extrinsicBlock = self.jac._createExtrinsicsJacobianBlock(intrinsicValues,
-                extrinsicValues, modelPoint)
+        extrinsicBlock = self.jac._createExtrinsicsJacobianBlock(self.intrinsicValues,
+                self.extrinsicValues, self.modelPoint)
         self.assertEqual(extrinsicBlock.shape, (2, 6))
         self.assertNoNans(extrinsicBlock)
         self.assertNonZero(extrinsicBlock)
@@ -44,6 +50,11 @@ class TestJacobian(unittest.TestCase):
     def test_compute(self):
         #self.jac.compute(P,
         pass
+
+    def test_getAllSymbols(self):
+        allSymbols = jacobian.getAllSymbolsOrdered()
+        self.assertIsInstance(allSymbols, tuple)
+        self.assertEqual(len(allSymbols), 19)
 
     def assertNoNans(self, Q):
         self.assertEqual(np.sum(np.isnan(Q)), 0)
